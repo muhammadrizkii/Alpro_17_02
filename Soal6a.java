af9ef1627420f8e06f8c7d378d4cb556dee1553c
muhammadrizkii <63945281+muhammadrizkii@users.noreply.github.com>
2020-04-25 19:43:15 +0700

    Delete Soal6a.java

--- Soal6a.java
+++
@@ -1,9 +0,0 @@
-package ch04;
-public class Soal6a {
-    public static void main(String[] args) {
-        int[] soal = {82, 12, 41, 38, 19, 26, 9, 48, 20, 55, 8, 32, 3};
-        for (int a = 0; a < soal.length; a++) {
-            System.out.print(soal[a]+" ");
-        }
-    }
-}
